fd109d92da61564c450f1a183b9cf94a5a0976ac
anupothenthomas <anupothenthomas@gmail.com>
2018-08-02 16:47:44 +0530

    without_join

--- src/thread_DOC/WithoutJoin.java
+++ src/thread_DOC/WithoutJoin.java
@@ -4,7 +4,27 @@ public class WithoutJoin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Thread th1 = new Thread(new MyClass(), "th1");
+		Thread th2 = new Thread(new MyClass(), "th2");
+		Thread th3 = new Thread(new MyClass(), "th3");
+
+		th1.start();
+		th2.start();
+		th3.start();
 
 	}
+}
 
+class MyClass implements Runnable {
+	@Override
+	public void run() {
+		Thread t = Thread.currentThread();
+		System.out.println("Thread Started" + t.getName());
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
+		}
+		System.out.println("Thread Ended" + t.getName());
+	}
 }
