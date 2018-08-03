8698b41a1554f1f70af96626854f77a37f3927c5
anupothenthomas <anupothenthomas@gmail.com>
2018-08-03 19:34:43 +0530

    AccessModifier-Public

---
+++ src/oops_EXTRAS_DOC/AddPublic.java
@@ -0,0 +1,10 @@
+package oops_EXTRAS_DOC;
+
+public class AddPublic 
+{
+	public int addnumbers(int a, int b)
+	{
+		return a+b;
+	}
+
+}
---
+++ src/oops_EXTRAS_DOC/AddPublicCall.java
@@ -0,0 +1,13 @@
+package oops_EXTRAS_DOC;
+
+public class AddPublicCall {
+
+	public static void main(String[] args) 
+	{
+		AddPublic obj = new AddPublic();
+		
+		System.out.println(obj.addnumbers(3, 4));
+		// TODO Auto-generated method stub
+	}
+
+}
