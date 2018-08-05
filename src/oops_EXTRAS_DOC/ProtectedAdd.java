27ea07fd79f85adcb2cc309c9d5685e161fe028a
anupothenthomas <anupothenthomas@gmail.com>
2018-08-05 14:23:11 +0530

    ProtectedAdd+Test

---
+++ src/oops_EXTRAS_DOC/ProtectedAdd.java
@@ -0,0 +1,10 @@
+package oops_EXTRAS_DOC;
+
+public class ProtectedAdd 
+{
+	protected int addNumbers(int a, int b)
+	{
+		return a+b;
+	}
+
+}
---
+++ src/oops_EXTRAS_DOC/ProtectedTest.java
@@ -0,0 +1,11 @@
+package oops_EXTRAS_DOC;
+
+public class ProtectedTest 
+{
+	public static void main(String[] args)
+	{
+		ProtectedAdd obj = new ProtectedAdd();
+		System.out.println("The result is:" +obj.addNumbers(2, 3));
+	}
+
+}
