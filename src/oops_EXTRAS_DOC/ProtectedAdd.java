3cdfbfe73d572e36109124e4e19d4c01b9ba39f8
anupothenthomas <anupothenthomas@gmail.com>
2018-08-05 14:26:39 +0530

    UserInput_ProtectedAdd+Test

--- src/oops_EXTRAS_DOC/ProtectedAdd.java
+++ src/oops_EXTRAS_DOC/ProtectedAdd.java
@@ -2,6 +2,7 @@ package oops_EXTRAS_DOC;
 
 public class ProtectedAdd 
 {
+	
 	protected int addNumbers(int a, int b)
 	{
 		return a+b;
--- src/oops_EXTRAS_DOC/ProtectedTest.java
+++ src/oops_EXTRAS_DOC/ProtectedTest.java
@@ -1,11 +1,18 @@
 package oops_EXTRAS_DOC;
 
+import java.util.Scanner;
+
 public class ProtectedTest 
 {
 	public static void main(String[] args)
 	{
+		Scanner n = new Scanner(System.in);
+		System.out.println("Enter the two numbers to be added:\n");
+		int a= n.nextInt();
+		int b= n.nextInt();
+		
 		ProtectedAdd obj = new ProtectedAdd();
-		System.out.println("The result is:" +obj.addNumbers(2, 3));
+		System.out.println("The result is: " +obj.addNumbers(a, b));
 	}
 
 }
